ed16695fe350e1b8ef5cb3d1a2648b7f34bf6719
Sloug <carrylol@aol.com>
2022-02-19 19:52:09 +0100

    Fixes inclusion of fsd_skeleton dependecy

--- src/amz_airsim_wrapper.cpp
+++ src/amz_airsim_wrapper.cpp
@@ -1,17 +1,17 @@
 #include <ros/ros.h>
-#include "fssd_common_msgs/ControlCommand"
+#include "fsd_common_msgs/ControlCommand.h"
 
-vosaddid controlCommand(const fssd_common_msgs/ControlCommand& msg)
+void controlCommand(const fsd_common_msgs::ControlCommand& msg)
 {
     ROS_INFO("control command triggered");
 }
 
-intsa main(int argc, char** argv)
+int main(int argc, char** argv)
 {
     ros::init(argc, argv, "wrapper");
     ros::NodeHandle nodeHandle;
     
-    ros::Subscriber subscriber = nodeHandle.subscribe("/control/pure_pursuit/control_command", 10, callback_function);
+    ros::Subscriber subscriber = nodeHandle.subscribe("/control/pure_pursuit/control_command", 10, controlCommand);
     
-    return0;
+    return 0;
 }
