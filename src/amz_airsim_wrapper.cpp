c40cd5e52c4f2ffca28bc248d68c6f5780a6e279
Sloug <carrylol@aol.com>
2022-02-20 16:39:05 +0100

    Subscribes all necessary topics

--- src/amz_airsim_wrapper.cpp
+++ src/amz_airsim_wrapper.cpp
@@ -1,9 +1,28 @@
 #include <ros/ros.h>
 #include "fsd_common_msgs/ControlCommand.h"
+#include "fsd_common_msgs/CarStateDt.h"
+#include "fsd_common_msgs/CarState.h"
+#include "fsd_common_msgs/Map.h"
+
 
 void controlCommand(const fsd_common_msgs::ControlCommand& msg)
 {
     ROS_INFO("control command triggered");
+}
+
+void velocityEstimate(const fsd_common_msgs::CarStateDt& msg)
+{
+    ROS_INFO("velocity estimate triggered");
+}
+
+void state(const fsd_common_msgs::CarState& msg)
+{
+    ROS_INFO("state triggered");
+}
+
+void map(const fsd_common_msgs::Map& msg)
+{
+    ROS_INFO("map triggered");
 }
 
 int main(int argc, char** argv)
@@ -11,7 +30,10 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "wrapper");
     ros::NodeHandle nodeHandle;
     
-    ros::Subscriber subscriber = nodeHandle.subscribe("/control/pure_pursuit/control_command", 10, controlCommand);
+    ros::Subscriber subscriber1 = nodeHandle.subscribe("/control/pure_pursuit/control_command", 10, controlCommand);
+    ros::Subscriber subscriber2 = nodeHandle.subscribe("/estimation/velocity_estimation/velocity_estimate", 10, velocityEstimate);
+    ros::Subscriber subscriber3 = nodeHandle.subscribe("/estimation/slam/state", 10, state);
+    ros::Subscriber subscriber4 = nodeHandle.subscribe("/estimation/slam/map", 10, map);
     
     return 0;
 }
