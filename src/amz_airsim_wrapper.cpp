108150ffdc62f14c477c75bd0b0b4cfde971c693
Sloug <carrylol@aol.com>
2022-02-24 17:22:32 +0100

    Adds break to switch case

--- src/amz_airsim_wrapper.cpp
+++ src/amz_airsim_wrapper.cpp
@@ -18,30 +18,10 @@ void controlCommand(const fsd_common_msgs::ControlCommand& msg)
 {
     fs_msgs::ControlCommand message;
     message.throttle = 5.0 * static_cast<double>(msg.throttle.data);
-    message.steering = 1.745329 * msg.steering_angle.data;
+    message.steering = -1.745329 * msg.steering_angle.data;
     message.brake = 0;
     controlCommandPublisher.publish(message);
 }
-
-// void velocityEstimate(const fsd_common_msgs::CarStateDt& msg)
-// {
-//     ROS_INFO("velocity estimate triggered");
-// }
-
-// void state(const fsd_common_msgs::CarState& msg)
-// {
-//     ROS_INFO("state triggered");
-// }
-
-// void map(const fsd_common_msgs::Map& msg)
-// {
-//     ROS_INFO("map triggered");
-// }
-
-// void controlCommandAS(const fs_msgs::ControlCommand& msg)
-// {
-//     ROS_INFO("control command AS triggered");
-// }
 
 void odom(const nav_msgs::Odometry& msg)
 {
@@ -78,19 +58,26 @@ void track(const fs_msgs::Track& msg)
             case 0: // BLUE
                 newCone.color.data = "BLUE";
                 map.cone_blue.push_back(newCone);
+                break;
             case 1: // YELLOW
                 newCone.color.data = "YELLOW";
                 map.cone_yellow.push_back(newCone);
+                break;
             case 2: // ORANGE BIG
+                break;
             case 3: // ORANGE SMALL
                 newCone.color.data = "ORANGE";
                 map.cone_orange.push_back(newCone);
+                break;
             case 4: // UNKNOWN
                 ROS_INFO("UNKNOWN CONE COLOR");
+                break;
             default:
                 ROS_INFO("INVALID CONE COLOR");
+                break;
         }
     }
+    ROS_INFO("Cone Blue %d", map.cone_blue.size());
     mapPublisher.publish(map);
 }
 
@@ -98,12 +85,8 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "wrapper");
     ros::NodeHandle nodeHandle;
-    ros::Subscriber controlCommandSubscriber = nodeHandle.subscribe("/control/pure_pursuit/control_command", 10, controlCommand);
-//     ros::Subscriber subscriber2 = nodeHandle.subscribe("/estimation/velocity_estimation/velocity_estimate", 10, velocityEstimate);
-//     ros::Subscriber subscriber3 = nodeHandle.subscribe("/estimation/slam/state", 10, state);
-//     ros::Subscriber subscriber4 = nodeHandle.subscribe("/estimation/slam/map", 10, map);
     
-//     ros::Subscriber subscriber5 = nodeHandle.subscribe("/fsds/control_command", 10, controlCommandAS);
+    ros::Subscriber controlCommandSubscriber = nodeHandle.subscribe("/control/pure_pursuit/control_command", 10, controlCommand);
     ros::Subscriber odomSubscriber = nodeHandle.subscribe("/fsds/testing_only/odom", 10, odom);
     ros::Subscriber trackSubscriber = nodeHandle.subscribe("/fsds/testing_only/track", 10, track);
     
@@ -112,9 +95,6 @@ int main(int argc, char** argv)
     statePublisher = nodeHandle.advertise<fsd_common_msgs::CarState>("/estimation/slam/state", 10);
     mapPublisher = nodeHandle.advertise<fsd_common_msgs::Map>("/estimation/slam/map", 10);
     
-//     ros::Publisher publisher3 = nodeHandle.advertise<nav_msgs::Odometry>("/fsds/testing_only/odom", 10);
-//     ros::Publisher publisher4 = nodeHandle.advertise<fs_msgs::Track>("/fsds/testing_only/track", 10);
-//     publisher1.publish(message);
     ros::spin();
 
     return 0;
